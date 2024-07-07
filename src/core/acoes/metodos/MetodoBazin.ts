6c0a55e1ce2cb5fde0af078b154b0c232c7d6807
PedroFernandoLima <pflfacul@gmail.com>
2024-07-07 15:01:02 -0400

    Adicionar método calcularMediaDividendos e métodos de validação em MetodoBazin.ts Remover função somar em index.ts Remover teste em index.test.ts

--- src/core/acoes/metodos/MetodoBazin.ts
+++ src/core/acoes/metodos/MetodoBazin.ts
@@ -1,22 +1,37 @@
 export default class MetodoBazin {
     static readonly DIVIDEND_YIELD_BAZIN: number = 0.06;
 
+
+    public static calcularMediaDividendos(valoresDividendosAnuais: number[]): number {
+        this.validarListagemDeDividendosPorAno(valoresDividendosAnuais);
+        const somaDividendos = valoresDividendosAnuais.reduce((acc, cur) => acc + cur, 0);
+        return somaDividendos / valoresDividendosAnuais.length;
+    }
+
     public static calcularPrecoTeto(mediaAnualDividendo: number, rendimentoEsperado: number = MetodoBazin.DIVIDEND_YIELD_BAZIN): number {
-        if (mediaAnualDividendo < 0) {
-            throw new Error("A média anual dos dividendos não pode ser negativa");
-        }
-        if (rendimentoEsperado <= 0) {
-            throw new Error("O rendimento esperado deve ser maior que zero");
-        }
+        this.validarMediaDeDividendoAnual(mediaAnualDividendo);
+        this.validarRendimentoEsperado(rendimentoEsperado);
+
         const precoTeto = mediaAnualDividendo / rendimentoEsperado;
+
         return parseFloat(precoTeto.toFixed(2));
     }
 
-    public static calcularMediaDividendos(valoresDividendosAnuais: number[]): number {
+    private static validarListagemDeDividendosPorAno(valoresDividendosAnuais: number[]) {
         if (valoresDividendosAnuais.length === 0) {
             throw new Error("A lista de dividendos anuais não pode ser vazia");
         }
-        const somaDividendos = valoresDividendosAnuais.reduce((acc, cur) => acc + cur, 0);
-        return somaDividendos / valoresDividendosAnuais.length;
+    }
+
+    private static validarRendimentoEsperado(rendimentoEsperado: number) {
+        if (rendimentoEsperado <= 0) {
+            throw new Error("O rendimento esperado deve ser maior que zero");
+        }
+    }
+
+    private static validarMediaDeDividendoAnual(mediaAnualDividendo: number) {
+        if (mediaAnualDividendo < 0) {
+            throw new Error("A média anual dos dividendos não pode ser negativa");
+        }
     }
 }
